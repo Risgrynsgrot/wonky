ea500a4ce380140b8e85effcffd37d867d34ec56
risgrynsgrot <kingminiman@gmail.com>
2024-03-03 12:10:57 +0100

    fixed gitignore

--- build/CPM_modules/Findraylib.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/risgrynsgrot/projects/wonky-c/build/cmake/CPM_0.38.6.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;raysan5/raylib;GIT_TAG;5.0;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(raylib_FOUND TRUE)
